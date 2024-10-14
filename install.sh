eed2bf89b2d59c3c936f93c099edb8a5d9ce8435
kabazoka <73285479+kabazoka@users.noreply.github.com>
2024-10-14 17:31:58 +0800

    Update install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+# install.sh
+# Install the evaluation tw_rouge package
+sudo apt-get install git
+git clone https://github.com/deankuo/ADL24-HW2.git
+cd ADL24-HW2
+pip install -e tw_rouge
+
+# Install the required packages
+conda install pytorch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 pytorch-cuda=12.1 -c pytorch -c nvidia
+pip intall -r requirements.txt
